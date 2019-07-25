9c12dd9f5db025540819fd75af4707ea381ada43
yodsapat sutthisasanakul <yodsapat.s@kerrylogistics.com>
2019-07-25 10:24:48 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+
+module.exports = {
+    plus: (first,second)=> first + second,
+    substract: (first,second)=> first - second
+}
---
+++ test.js
@@ -0,0 +1,3 @@
+const cal = require('./index');
+console.log(cal.plus(10,5));
+console.log(cal.substract(4,1));
